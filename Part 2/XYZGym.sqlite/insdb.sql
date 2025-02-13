36e538103937d672dde9f723704349171d9ff07d
hagekn <hannah.knight@enmu.edu>
2025-02-13 11:14:59 -0700

    Update insdb.sql

--- Part 2/XYZGym.sqlite/insdb.sql
+++ Part 2/XYZGym.sqlite/insdb.sql
@@ -2,7 +2,7 @@
 INSERT INTO Member (name, email, phone, address, age, membershipStartDate, membershipEndDate) VALUES
 ('John Doe', 'johnd@email.com', '1234567890', '123 Main St', 25, '2024-01-01', '2025-01-01'),
 ('Jane Smith', 'janes@email.com', '9876543210', '456 Oak St', 30, '2024-02-01', '2025-02-01'),
-('Alice Johnson', 'alicej@email.com', '5556667777', '789 Pine St', 40, '2024-03-01', '2025-03-01'),
+('Alice Johnson', 'alicej@email.com', '5556667777', '789 Pine St', 40, '2024-03-01', '2024-04-01'),
 ('Bob Brown', 'bobb@email.com', '2223334444', '321 Street St', 35, '2024-04-01', '2025-04-01'),
 ('Maria Garcia', 'mariag@email.com', '9998887777', '654 Green St', 28, '2024-05-01', '2025-05-01');
 
@@ -29,3 +29,32 @@ INSERT INTO Class (className, classType, duration, classCapacity, instructorId, 
 ('Strength Training', 'Weight-Lifting', 90, 15, 3, 3),
 ('Acrobatics Basics', 'Acrobatics', 75, 10, 4, 4),
 ('Advanced Cardio', 'Cardio', 50, 30, 5, 5);
+
+-- Insert Equipment records
+INSERT INTO Equipment (name, type, quantity, gymId) VALUES
+('Treadmill', 'Cardio', 5, 2),
+('Barbell', 'Strength', 2, 3),
+('Resistance Band', 'Flexibility', 20, 4),
+('Foam Roller', 'Recovery', 2, 1),
+('Stair Climber', 'Cardio', 10, 5);
+
+-- Insert MembershipPlan records
+INSERT INTO MembershipPlan (planType, cost) VALUES
+('Monthly', 20),
+('Annual', 200);
+
+-- Insert Payment records
+INSERT INTO Payment (memberId, planId, amountPaid, paymentDate) VALUES
+(1, 1, 200, '2024-01-01'),
+(2, 1, 200, '2024-02-01'),
+(3, 2, 20, '2024-03-01'),
+(4, 1, 200, '2024-04-01'),
+(5, 2, 200, '2024-05-01');
+
+-- Insert Attendence measures
+INSERT INTO Attendance (memberId, classId, attendanceDate) VALUES
+(1, 2, '2024-01-03'),
+(2, 4, '2024-04-21'),
+(3, 5, '2024-03-14'),
+(4, 3, '2024-11-19'),
+(5, 1, '2024-07-14');
